9347f161101a485243a78e8c2252d4791c6712a1
Platine <platinetwitch@gmail.com>
2021-01-15 16:57:01 +0100

    Code propre, need connection DB

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,16 +1,21 @@
 import os
-
 from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
 
+    # Des trucs qui seront surrement la
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:user@localhost/Agate'
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # db = SQLAlchemy(app)
+
+    app.config.from_mapping(SECRET_KEY='dev')
+
+    # -------------------------- #
+    # ---------- Test ---------- #
+    # -------------------------- #
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
@@ -24,6 +29,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    # ---------------------------- #
+    # ---------- Routes ---------- #
+    # ---------------------------- #
     @app.route('/')
     def index():
         return render_template('index.html')
