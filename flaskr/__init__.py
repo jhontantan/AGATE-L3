391e1d5794d130f329f845e1ecb538be1f3d08b9
Platine <platinetwitch@gmail.com>
2021-01-19 11:11:37 +0100

    up

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,21 +1,12 @@
 import os
 from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
-    # Des trucs qui seront surrement la
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:user@localhost/Agate'
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # db = SQLAlchemy(app)
-
     app.config.from_mapping(SECRET_KEY='dev')
 
-    # -------------------------- #
-    # ---------- Test ---------- #
-    # -------------------------- #
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
@@ -29,9 +20,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # ---------------------------- #
-    # ---------- Routes ---------- #
-    # ---------------------------- #
     @app.route('/')
     def index():
         return render_template('index.html')
