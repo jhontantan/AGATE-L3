2f6a1bcd9b81fa428cbd41e8a406a44fb060b2f5
Elodie D <elodie.deflon@gmail.com>
2021-02-02 10:33:58 +0100

    Ajout "ANNEE", fin responsive
    - Fonts responsive 
    - Ajout d'un champ "annee" pour l'import
    - Correction d'un bug sur une alerte (ne se fermait pas quand on cliquait sur la croix)

--- static/script.js
+++ static/script.js
@@ -1,21 +1,20 @@
-  function myFunction() {
-            var x = document.getElementById("hideImport");
-            if (x.style.display === "none") {
-                x.style.display = "block";
-            } else {
-                x.style.display = "none";
-            }
-        }
-  (function () {
-
-        var $alertMsg = $("#alertMsg");
-
-        function configListeners() {
-
-
-        $alertMsg.on("close.bs.alert", function () {
-            $alertMsg.hide();
-            return false;
-        });
+function myFunction() {
+    var x = document.getElementById("hideImport");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
-    })();
+}
+
+(function () {
+
+    var $alertMsg = $("#alertMsg");
+
+    // function configListeners() {
+    //     $alertMsg.on("close.bs.alert", function () {
+    //         $alertMsg.hide();
+    //         return false;
+    //     });
+    // }
+})();
