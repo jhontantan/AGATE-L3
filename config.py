491118c5029f3cdf076d0a534475ae06a5d26f22
Platine <platinetwitch@gmail.com>
2021-05-14 14:32:57 +0200

    Fix PostMerge

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     DB_USER = "postgres"
     DB_PASS = "user"
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://" + DB_USER + ":" + DB_PASS + "@" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME
-    SQLALCHEMY_TRACK_MODIFICATIONS = '123456789'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # ----- Import ----- #
 
@@ -19,18 +19,18 @@ class Config:
     MAIL_SERVER = 'smtp.office365.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USE_SSL = '123456789'
+    MAIL_USE_SSL = False
 
     MAIL_USERNAME = 'adressedetest73@outlook.fr' # 'traitement-geomatique@agate-territoires.fr'
-    MAIL_PASSWORD = 'agate123' # 'Won06597'
+    MAIL_PASSWORD = 'motdepasse73' # 'Won06597'
 
     # Options pour le debug
     MAIL_DEBUG = True
-    MAIL_SUPPRESS_SEND = '123456789'
+    # MAIL_SUPPRESS_SEND = False
 
     MAIL_DEFAULT_SENDER = ('Outil Agate', 'adressedetest73@outlook.fr') # ('Outil Agate', 'traitement-geomatique@agate-territoires.fr')
     MAIL_MAX_EMAILS = None
     MAIL_ASCII_ATTACHMENTS = True
 
-    MAIL_ADRESSES_DEST = ['jhontantan@hotmail.es', 'jomar404@gmail.com']  # geomatique@agate-territoires.fr
+    MAIL_ADRESSES_DEST = ['adressedetest73@outlook.fr']  # geomatique@agate-territoires.fr
     ADMIN_PASSWORD = '123456789b'
