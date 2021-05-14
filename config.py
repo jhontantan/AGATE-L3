f7b7beb3cf43013ce5eb453328a971dc03aed703
Jonathan <jomar404@gmail.com>
2021-05-14 16:19:00 +0200

    mdp 123456
    ;

--- config.py
+++ config.py
@@ -33,4 +33,4 @@ class Config:
     MAIL_ASCII_ATTACHMENTS = True
 
     MAIL_ADRESSES_DEST = ['adressedetest73@outlook.fr']  # geomatique@agate-territoires.fr
-    ADMIN_PASSWORD = '25d55ad283aa400af464c76d713c07ad'
+    ADMIN_PASSWORD = 'e10adc3949ba59abbe56e057f20f883e' #123456
